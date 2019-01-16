80d7537a997e3d961fc23feddea3b7efc94c85b3
dongyi <juvenpp@gmail.com>
2019-01-16 12:17:32 +0800

    Update constants.py

--- bitmex_websocket/constants.py
+++ bitmex_websocket/constants.py
@@ -1,4 +1,4 @@
-from enum import Enum, auto
+from enum import Enum
 
 
 class NoValue(Enum):
@@ -10,84 +10,83 @@ class BaseChannels(NoValue):
 
 class Channels(BaseChannels):
     # Trollbox chat
-    chat = auto()
+    chat = 1
 
     # Statistics of connected users/bots
-    connected = auto()
+    connected = 2
 
     # Daily Insurance Fund updates
-    insurance = auto()
+    insurance = 3
 
     # Liquidation orders as they're entered into the book
-    liquidation = auto()
+    liquidation = 4
 
     # System-wide notifications
-    publicNotifications = auto()
+    publicNotifications = 5
 
 
 class SecureChannels(BaseChannels):
     # Affiliate status, such as total referred users & payout %
-    affiliate = auto()
+    affiliate = 6
 
     # Updates on your current account balance and margin requirements
-    margin = auto()
+    margin = 7
 
     # Individual notifications - currently not used
-    privateNotifications = auto()
+    privateNotifications = 8
 
     # Bitcoin address balance data, including total deposits & withdrawals
-    wallet = auto()
+    wallet = 9
 
     # Deposit/Withdrawal updates
-    transact = auto()
+    transact = 10
 
 
 class InstrumentChannels(BaseChannels):
     # Instrument updates including turnover and bid/ask
-    instrument = auto()
+    instrument = 11
 
     # Top 10 levels using traditional full book push
-    orderBook10 = auto()
+    orderBook10 = 12
 
     # Full level 2 orderBook
-    orderBookL2 = auto()
+    orderBookL2 = 13
 
     # Top level of the book
-    quote = auto()
+    quote = 14
 
     # 1-minute quote bins
-    quoteBin1m = auto()
+    quoteBin1m = 15
 
     # Settlements
-    settlement = auto()
+    settlement = 16
 
     # Live trades
-    trade = auto()
+    trade = 17
 
     # 1-minute ticker bins
-    tradeBin1m = auto()
+    tradeBin1m = 18
 
 
 class SecureInstrumentChannels(BaseChannels):
     # Individual executions; can be multiple per order
-    execution = auto()
+    execution = 19
 
     # Live updates on your orders
-    order = auto()
+    order = 20
 
     # Updates on your positions
-    position = auto()
+    position = 21
 
 
 class Action(NoValue):
-    partial = auto()
+    partial = 22
 
-    insert = auto()
+    insert = 23
 
-    update = auto()
+    update = 24
 
-    delete = auto()
-
+    delete = 25
 
 # Don't grow a table larger than this amount. Helps cap memory usage.
 MAX_TABLE_LEN = 200
