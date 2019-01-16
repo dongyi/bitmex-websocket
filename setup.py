203eebb96c0c91665869eaeb81f70c707d1f4890
dongyi <juvenpp@gmail.com>
2019-01-16 12:04:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from pip.download import PipSession
 from os.path import realpath
 
