43df6720421e75a6669aa6a3fd95dc37bb97ac78
dongyi <juvenpp@gmail.com>
2019-01-16 12:06:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@
 from setuptools import setup
 try: # for pip >= 10
     from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
 except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
-
-from pip.download import PipSession
+    from pip.download import PipSession
+    
 from os.path import realpath
 
 
