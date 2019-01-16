36f79073d3e71f0f94a2a8b5b74d88dec1113b81
dongyi <juvenpp@gmail.com>
2019-01-16 10:43:12 +0800

    Update _instrument.py
    兼容py3.5

--- bitmex_websocket/_instrument.py
+++ bitmex_websocket/_instrument.py
@@ -40,7 +40,7 @@ class Instrument(BitMEXWebsocket):
 
     def subscribe_channels(self):
         for channel in self.channels:
-            channel_key = f'{channel.name}:{self.symbol}'
+            channel_key = '{}:{}'.format(channel.name, self.symbol)
             self.subscribe(channel_key)
 
     def on_action(self, message):
